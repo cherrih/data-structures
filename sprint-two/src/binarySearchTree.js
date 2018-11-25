bcad9fd7402fbeba636a57c492ddeef9d9d3b3bc
cherrih <cherri.hartigan@gmail.com>
2018-11-24 17:38:05 -0800

    Refactor Bare Minimum Reqs to ES6 classes

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,85 +1,63 @@
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  } 
 
-var BinarySearchTree = function(value) {
-  var newBinaryTree = {};
-  newBinaryTree.value = value;
-  
-  newBinaryTree.left = null;
-  newBinaryTree.right = null;
-  
-  extend(newBinaryTree, binaryTreeMethods);
-  
-  return newBinaryTree;
-};
+  // O(log n)
+  insert (value) {
+    var child = new BinarySearchTree(value);
 
-var extend = function(to, from) {
-  for (var key in from) {
-    to[key] = from[key];
-  }
-};
-
-var binaryTreeMethods = {};
-
-// O(log n)
-binaryTreeMethods.insert = function(value) {
-  var child = BinarySearchTree(value);
-
-  if (value < this.value) {
-    if (this.left === null) {
-      this.left = child;
-    } else {
-      this.left.insert(value);
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = child;
+      } else {
+        this.left.insert(value);
+      }
+    }
+    
+    if (value > this.value) {
+      if (this.right === null) {
+        this.right = child;
+      } else {
+        this.right.insert(value);
+      }
     }
   }
-  
-  if (value > this.value) {
-    if (this.right === null) {
-      this.right = child;
-    } else {
-      this.right.insert(value);
+
+  //O(log n)
+  contains (value) {
+    if (this.value === value) {
+      return true;
+    }
+    
+    if (value < this.value) {
+      if (this.left === null) {
+        return false;
+      } else {
+        return this.left.contains(value);
+      }
+    }
+    
+    if (value > this.value) {
+      if (this.right === null) {
+        return false;
+      } else {
+        return this.right.contains(value); 
+      }
     }
   }
-};
 
-//O(log n)
-binaryTreeMethods.contains = function(value) {
-  if (this.value === value) {
-    return true;
-  }
-  
-  if (value < this.value) {
-    if (this.left === null) {
-      return false;
-    } else {
-      return this.left.contains(value);
+  //O(n)
+  depthFirstLog (cb) {
+    cb (this.value);
+    if (this.left) {
+      this.left.depthFirstLog(cb);
+    }
+    if (this.right) {
+      this.right.depthFirstLog(cb);
     }
   }
-  
-  if (value > this.value) {
-    if (this.right === null) {
-      return false;
-    } else {
-      return this.right.contains(value); 
-    }
-  }
-};
 
-//O(n)
-binaryTreeMethods.depthFirstLog = function(cb) {
-  cb (this.value);
-  if (this.left) {
-    this.left.depthFirstLog(cb);
-  }
-  if (this.right) {
-    this.right.depthFirstLog(cb);
-  }
-  
-};
-
-
-
-
-
-
-
-
-
+}
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,30 +1,27 @@
-var Set = function() {
-  var set = Object.create(setPrototype);
-  set.storage = {};
-  // set.count = 0
-  return set;
+class Set {
+  constructor() {
+    this.storage = {};
+  };
+
+  // O(1)
+  add (item) {
+    this.storage[item] = item;
+  };
+
+  // O(1)
+  contains (item) {
+    if (item in this.storage) {
+      return true;
+    }
+    return false;
+  };
+
+  // O(1)
+  remove (item) {
+    delete this.storage[item];
+  };
+
 };
-
-var setPrototype = {};
-
-// O(1)
-setPrototype.add = function(item) {
-  this.storage[item] = item;
-};
-
-// O(1)
-setPrototype.contains = function(item) {
-  if (item in this.storage) {
-    return true;
-  }
-  return false;
-};
-
-// O(1)
-setPrototype.remove = function(item) {
-  delete this.storage[item];
-};
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
